667e22604c3c86b93bdf1f16dc1e39620af52f66
EtienneDumai <etienne.dumai@gmail.com>
2025-09-25 10:36:26 +0200

    ajout du BeforeEach

--- tdtp1/Calculator/src/main/java/com/dumai/Calculator/Calculator.java
+++ tdtp1/Calculator/src/main/java/com/dumai/Calculator/Calculator.java
@@ -1,4 +1,12 @@
 package com.dumai.Calculator;
 
 public class Calculator {
+    Calculator(){}
+    public int add(int opG, int opD){
+        return opG + opD;
+    }
+    public int divide(int opG, int opD){
+        return opG / opD;
+    }
+
 }
--- tdtp1/Calculator/src/test/java/com/dumai/Calculator/CalculatorTest.java
+++ tdtp1/Calculator/src/test/java/com/dumai/Calculator/CalculatorTest.java
@@ -1,4 +1,20 @@
 package com.dumai.Calculator;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 public class CalculatorTest {
+    private Calculator c;
+    @BeforeEach
+    void setUp() {
+        c =  new Calculator();
+    }
+    @Test
+    public void devrait_additioner_sans_erreur(){
+        assertThat(c.add(1,2)).isEqualTo(3);
+    }
+    @Test
+    public void devrait_diviser_sans_erreur(){
+        assertThat(c.divide(10,2)).isEqualTo(5);
+    }
 }
