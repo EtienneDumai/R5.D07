b4470f8cec2b14f30f1d5f311a27fa35edffd213
EtienneDumai <etienne.dumai@gmail.com>
2025-09-25 12:24:35 +0200

    debut du set

--- tdtp1/Calculator/src/main/java/com/dumai/Calculator/Calculator.java
+++ tdtp1/Calculator/src/main/java/com/dumai/Calculator/Calculator.java
@@ -1,5 +1,8 @@
 package com.dumai.Calculator;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Calculator {
     Calculator(){}
     public int add(int opG, int opD){
@@ -7,6 +10,21 @@ public class Calculator {
     }
     public int divide(int opG, int opD){
         return opG / opD;
+    }
+    public Set<Integer> ensembleschiffres(int pNombre){
+        Set<Integer> res = new HashSet<>();
+        long n = Math.abs((long) pNombre); // évite le débordement pour Integer.MIN_VALUE
+
+        if (n == 0) {
+            res.add(0);
+            return res;
+        }
+        while (n > 0) {
+            res.add((int) (n % 10));
+            n /= 10;
+        }
+        return res;
+
     }
 
 }
