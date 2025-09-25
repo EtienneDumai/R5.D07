db573a1bea3d6d36cb4ebd29dc8db3d392d82ea9
EtienneDumai <etienne.dumai@gmail.com>
2025-09-25 10:53:22 +0200

    Paramteredtest pass

--- tdtp1/Calculator/src/test/java/com/dumai/Calculator/CalculatorTest.java
+++ tdtp1/Calculator/src/test/java/com/dumai/Calculator/CalculatorTest.java
@@ -2,6 +2,9 @@ package com.dumai.Calculator;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.assertj.core.api.Assertions.assertThat;
 public class CalculatorTest {
     private Calculator c;
@@ -16,5 +19,16 @@ public class CalculatorTest {
     @Test
     public void devrait_diviser_sans_erreur(){
         assertThat(c.divide(10,2)).isEqualTo(5);
+    }
+    @ParameterizedTest(name = "{0}, {1}, {2}")
+    @CsvSource({
+            "0, 1, 1",
+            "1, 2, 3",
+            "-2, 2, 0",
+            "0, 0, 0",
+            "-1, -2, -3"
+    })
+    public void devrait_additioner_avec_valeurs_parametrees(int opG, int opD, int resultatAttendu) {
+        assertThat(c.add(opG, opD)).isEqualTo(resultatAttendu);
     }
 }
