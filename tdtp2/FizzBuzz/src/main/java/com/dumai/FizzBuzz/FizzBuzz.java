10f1cc7413db30ffe58f46a752885db8366c18dc
EtienneDumai <etienne.dumai@gmail.com>
2025-09-30 16:09:20 +0200

    debut de premier test de fizzuzz

--- tdtp2/FizzBuzz/src/main/java/com/dumai/FizzBuzz/FizzBuzz.java
+++ tdtp2/FizzBuzz/src/main/java/com/dumai/FizzBuzz/FizzBuzz.java
@@ -1,4 +1,15 @@
 package com.dumai.FizzBuzz;
 
+
 public class FizzBuzz {
+    public FizzBuzz() {
+    }
+
+    public static String de(int nb) {
+        String s = "";
+        if (nb % 3 == 0) {
+            s = "Fizz";
+        }
+        return s;
+    }
 }
--- tdtp2/FizzBuzz/src/test/java/com/dumai/FizzBuzzTest/FizzBuzzTest.java
+++ tdtp2/FizzBuzz/src/test/java/com/dumai/FizzBuzzTest/FizzBuzzTest.java
@@ -1,4 +1,21 @@
 package com.dumai.FizzBuzzTest;
 
+import com.dumai.FizzBuzz.FizzBuzz;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class FizzBuzzTest {
+    private FizzBuzz f;
+    private String expected;
+    @BeforeEach
+    public void setup() {
+        f = new FizzBuzz();
+    }
+    @Test
+    public void devrait_retourner_fizz(){
+        expected = "Fizz";
+        assertThat(f.de(3)).isEqualTo(expected);
+    }
 }
